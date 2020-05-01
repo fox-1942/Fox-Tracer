ff672abcea172484670f5302d6ca62a1ce6f6b3e
fox-1942 <mail.fox2@yahoo.com>
2020-05-01 23:50:06 +0200

    Fixes in bvh4.

--- includes/bvhtree.h
+++ includes/bvhtree.h
@@ -10,6 +10,10 @@
 #include <vector>
 #include "../Vendor/glm/vec3.hpp"
 
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+#define MAX(A, B) ((A) > (B) ? (A) : (B))
+
+
 using namespace std;
 
 struct BBox {
@@ -18,20 +22,31 @@ struct BBox {
     glm::vec3 center;
     int longestAxis;
 
-    float length = max.x - min.x;
-    float width = max.y - min.y;
-    float height = max.z - min.z;
+
+
+    BBox() {}
 
     BBox(glm::vec3 min, glm::vec3 max, glm::vec3 center) {
         this->min = min;
         this->max = max;
         this->center = center;
-        if (length > width && length > height) longestAxis = 0;
-        else if (width > length && width > height) longestAxis = 1;
-        else longestAxis = 2;
+
+        float length = max.x - min.x;
+        float width = max.y - min.y;
+        float height = max.z - min.z;
+
+        cout<<"min: " << this->min.x<< " " <<this->min.y<<" " <<this->min.z <<endl;
+        cout<<"max: " << this->max.x<< " " <<this->max.y<<" " <<this->max.z <<endl;
+        cout<<"center: " << this->center.x<< " " <<this->center.y<<" " <<this->center.z <<endl;
+        cout<<"length: " << length<<endl;
+
+
+        if (length >= width && length >= height) {longestAxis = 0;}
+        else if (width >= length && width >= height) {longestAxis = 1;}
+        else {longestAxis = 2;};
     }
 
-    BBox() {}
+
 };
 
 class BvhNode {
@@ -47,7 +62,7 @@ public:
     BBox bBox;
     BvhNode *right;
     BvhNode *left;
-    int numberofBBoxes=0;
+    int numberofBBoxes = 0;
 
     BvhNode(vector<glm::vec3> &primitiveCoordinates) {
         this->primitiveCoordinates = primitiveCoordinates;
@@ -56,51 +71,78 @@ public:
     }
 
     glm::vec3 calculateCenterofTriangle(glm::vec3 vec, glm::vec3 vec1, glm::vec3 vec2) {
-        return glm::vec3((vec.x + vec1.x + vec2.x) / 3, (vec.y + vec1.y + vec2.y) / 3, (vec.z + vec1.z + vec2.z) / 3);
+        return glm::vec3(float((vec.x + vec1.x + vec2.x) / 3), float((vec.y + vec1.y + vec2.y) / 3),
+                         float((vec.z + vec1.z + vec2.z) / 3));
     }
 
 
     BvhNode *buildTree(vector<glm::vec3> &indicesPerFaces, int depth) {
-        BvhNode* node=new BvhNode();
+        BvhNode *node = new BvhNode();
 
-        node->left=NULL;
-        node->right=NULL;
-        node->bBox=BBox();
+
+        node->bBox = BBox();
 
         if (indicesPerFaces.size() == 0 || primitiveCoordinates.size() == 0) return node;
 
-        if(indicesPerFaces.size()==1){
-            node->bBox=BBox();
-            node->left=new BvhNode();
-            node->right=new BvhNode();
+        if (indicesPerFaces.size() == 1) {
             return node;
         }
 
-        node->bBox=getBBox(indicesPerFaces);
+        node->bBox = getBBox(indicesPerFaces);
 
-
-        bBox = getBBox(indicesPerFaces);
-
-        int axis = bBox.longestAxis;
+        int axis = node->bBox.longestAxis;
 
 
         vector<glm::vec3> leftTree;
         vector<glm::vec3> rightTree;
-/*
-        cout << "face-ek száma: " << indicesPerFaces.size() << endl;
-        cout << "faceCenterek-ek száma: " << faceCenters.size() << endl;
 
-        cout << "bbox center: " << bBox.center.x<< " "<<bBox.center.y <<" "<< bBox.center.z << endl;
-        cout << "bbox center: " << bBox.center[axis] << endl;
-*/
+
+        glm::vec3 firstCoordofTri;
+        glm::vec3 secondCoordofTri;
+        glm::vec3 thirdCoordofTri;
+        vector<glm::vec3> currentTri;
+
         for (int i = 0; i < indicesPerFaces.size(); ++i) {
-            if (bBox.center[axis] >= faceCenters.at(i)[axis]) {
-               // cout<< faceCenters.at(i)[axis] <<endl;
-                leftTree.push_back(indicesPerFaces.at(i));
+            firstCoordofTri = getCoordinatefromIndices(indicesPerFaces.at(i).x);
+            secondCoordofTri = getCoordinatefromIndices(indicesPerFaces.at(i).y);
+            thirdCoordofTri = getCoordinatefromIndices(indicesPerFaces.at(i).z);
 
-            } else if (bBox.center[axis] <= faceCenters.at(i)[axis]) {
-               // cout<< faceCenters.at(i)[axis] <<endl;
-                rightTree.push_back(indicesPerFaces.at(i));
+            currentTri.push_back(firstCoordofTri);
+            currentTri.push_back(secondCoordofTri);
+            currentTri.push_back(thirdCoordofTri);
+
+            glm::vec3 centerOfCurrTri(calculateCenterofTriangle(firstCoordofTri, secondCoordofTri, thirdCoordofTri));
+
+            cout << bBox.center.x << " " << bBox.center.x << " " << bBox.center.x << endl;
+            cout << centerOfCurrTri.x << " " << centerOfCurrTri.x << " " << centerOfCurrTri.x << endl;
+
+            cout << "Axis: " << axis << endl;
+
+            switch (axis) {
+                case 0:
+                    if (bBox.center.x >= centerOfCurrTri.x) {
+                        leftTree.push_back(indicesPerFaces.at(i));
+
+                    } else if (bBox.center.x <= centerOfCurrTri.x) {
+                        rightTree.push_back(indicesPerFaces.at(i));
+                    }
+                    break;
+                case 1:
+                    if (bBox.center.y >= centerOfCurrTri.y) {
+                        leftTree.push_back(indicesPerFaces.at(i));
+
+                    } else if (bBox.center.y <= centerOfCurrTri.y) {
+                        rightTree.push_back(indicesPerFaces.at(i));
+                    }
+                    break;
+                case 2:
+                    if (bBox.center.z >= centerOfCurrTri.z) {
+                        leftTree.push_back(indicesPerFaces.at(i));
+
+                    } else if (bBox.center.z <= centerOfCurrTri.z) {
+                        rightTree.push_back(indicesPerFaces.at(i));
+                    }
+                    break;
             }
         }
 
@@ -110,26 +152,30 @@ public:
         }
 
         depth++;
-        cout<<"depth: "<<depth<<endl;
+        cout << "depth: " << depth << endl;
         this->left = buildTree(leftTree, depth);
         this->right = buildTree(rightTree, depth);
 
-       // cout<<"Boxok száma: "<<numberofBBoxes;
+        // cout<<"Boxok száma: "<<numberofBBoxes;
         return this;
     }
 
     glm::vec3 getCoordinatefromIndices(float index) {
-        return primitiveCoordinates.at((int) index);
+        /*  cout << "getCoordinates: " << endl;
+          cout << primitiveCoordinates.at(index).x << " " << primitiveCoordinates.at(index).y << " "
+               << primitiveCoordinates.at(index).z << endl;*/
+
+        return primitiveCoordinates.at(index);
     }
 
 
-    BBox getBBox(vector<glm::vec3>& indices) {
+    BBox getBBox(vector<glm::vec3> &indices) {
         this->numberofBBoxes++;
         glm::vec3 center(0, 0, 0);
 
-        float minX = 1e9, maxX = -1e9;
-        float minY = 1e9, maxY = -1e9;
-        float minZ = 1e9, maxZ = -1e9;
+        float minX = 999999999, maxX = -999999;
+        float minY = 999999999, maxY = -999999;
+        float minZ = 999999999, maxZ = -999999;
 
         glm::vec3 firstCoordofTri;
         glm::vec3 secondCoordofTri;
@@ -146,25 +192,14 @@ public:
             currentTri.push_back(thirdCoordofTri);
 
             for (int j = 0; j < currentTri.size(); j++) {
-                if (currentTri.at(j).x < minX) {
-                    minX = currentTri.at(j).x;
-                }
-                if (currentTri.at(j).y < minY) {
-                    minY = currentTri.at(j).y;
-                }
-                if (currentTri.at(j).z < minZ) {
-                    minZ = currentTri.at(j).z;
-                }
 
-                if (currentTri.at(j).x > maxX) {
-                    maxX = currentTri.at(j).x;
-                }
-                if (currentTri.at(j).y > maxY) {
-                    maxY = currentTri.at(j).y;
-                }
-                if (currentTri.at(j).z > maxZ) {
-                    maxZ = currentTri.at(j).z;
-                }
+                minX = MIN(currentTri.at(j).x, minX);
+                minY = MIN(currentTri.at(j).y, minY);
+                minZ = MIN(currentTri.at(j).z, minZ);
+
+                maxX = MAX(currentTri.at(j).x, maxX);
+                maxY = MAX(currentTri.at(j).y, maxY);
+                maxZ = MAX(currentTri.at(j).z, maxZ);
             }
 
             glm::vec3 centerOfCurrTri(calculateCenterofTriangle(firstCoordofTri, secondCoordofTri, thirdCoordofTri));
@@ -178,14 +213,13 @@ public:
         glm::vec3 minp(minX, minY, minZ);
         glm::vec3 maxp(maxX, maxY, maxZ);
 
-       /* cout << center.x << " " << center.y << " " << center.z << endl;
+        /* cout << center.x << " " << center.y << " " << center.z << endl;
 
-        cout << "Min of bbox: " << minp.x << " " << minp.y << " " << minp.z << endl;
-        cout << "Max of BBox: " << maxp.x << " " << maxp.y << " " << maxp.z << endl;*/
+         cout << "Min of bbox: " << minp.x << " " << minp.y << " " << minp.z << endl;
+         cout << "Max of BBox: " << maxp.x << " " << maxp.y << " " << maxp.z << endl;*/
 
         return BBox(minp, maxp, center);
     }
-
 
 
 };
